3ef6a4bd23ec2bc32a8331fc42217992a3cd9df2
DanielDDG <DanielGonzalezDDG@outlook.com>
2023-01-14 18:46:54 -0500

    Create header and github tag

---
+++ etchsketch.js
@@ -0,0 +1,12 @@
+/*
+
+End Goal:
+
+Create an Etch-A-Sketch program for a web browser.
+
+Problems:
+
+1. Create a webpage with a 16x16 grid of square divs (create them with JS). It's best to put
+them in a div container which can go directly into your HTML file.
+
+*/
