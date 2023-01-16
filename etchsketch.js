ca0aa407558b1c3da59f906f0d63f132e4db254a
DanielDDG <DanielGonzalezDDG@outlook.com>
2023-01-15 20:14:40 -0500

    Complete eventlistener functionality

--- etchsketch.js
+++ etchsketch.js
@@ -18,38 +18,51 @@ const square = document.createElement('div');
 square.classList.add('square');
 container.appendChild(square);
 
+// Drawing variable.
+
+let drawing = false;
 createGrid(16);
 
-/* Creating other divs for testing.
-
-const square1 = document.createElement('div');
-square1.classList.add('square');
-container.appendChild(square1);
-
-const square2 = document.createElement('div');
-square2.classList.add('square');
-container.appendChild(square2);
-
-const square3 = document.createElement('div');
-square3.classList.add('square');
-container.appendChild(square3);
-
-const square4 = document.createElement('div');
-square4.classList.add('square');
-container.appendChild(square4);
-
-*/
 
 // Function that if user enters number, it creates that number of divs ^2.
 
 function createGrid(input) {
 
+    square.addEventListener('mousedown', () => {
+        square.setAttribute('style', 'background-color: black;');
+        drawing = true;
+    });
+
+    square.addEventListener('mousemove', () => {
+        if (drawing) {
+            square.setAttribute('style', 'background-color: black;');
+        }
+    });
+
+    square.addEventListener('mouseup', () => {
+        drawing = false;
+    });
+      
     const squares = [input];
+
     for (let i = 0; i <= input * input - 2; i++) {
         squares[i] = document.createElement('div');
         squares[i].classList.add('square');
         container.appendChild(squares[i]);
-    }
+
+        squares[i].addEventListener('mousedown', () => {
+            squares[i].setAttribute('style', 'background-color: black;');
+            drawing = true;
+        });
+
+        squares[i].addEventListener('mousemove', () => {
+            if (drawing) {
+                squares[i].setAttribute('style', 'background-color: black;');
+            }
+        });
+
+        squares[i].addEventListener('mouseup', () => {
+            drawing = false;
+        });
+    }   
 }
-
-
