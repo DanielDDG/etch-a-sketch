2dea6240403ac2f793adcfa27295723bd08b0d6e
DanielDDG <DanielGonzalezDDG@outlook.com>
2023-01-15 23:54:13 -0500

    Create setDimension function

--- etchsketch.js
+++ etchsketch.js
@@ -1,28 +1,24 @@
-/*
-
-End Goal:
-
-Create an Etch-A-Sketch program for a web browser.
-
-Problems:
-
-1. Create a webpage with a 16x16 grid of square divs (create them with JS). It's best to put
-them in a div container which can go directly into your HTML file.
-
-*/
-
-// This is to create a square div.
+// Creates HTML elements and initializes variables.
 
 const container = document.querySelector('.container');
+const button = document.querySelector('button');
 const square = document.createElement('div');
+const allSquares = document.getElementsByClassName('square');
+let drawing = false;
 square.classList.add('square');
 container.appendChild(square);
 
-// Drawing variable.
+// Function that prompts user to set dimensions.
 
-let drawing = false;
-createGrid(16);
+function setDimension() {
 
+    let input = prompt('How many boxes would you like on each side?')
+    if (input > 100) {
+        alert('Please enter a number from 1 to 100.')
+    } else {
+        createGrid(input);
+    }
+}
 
 // Function that if user enters number, it creates that number of divs ^2.
 
@@ -32,37 +28,44 @@ function createGrid(input) {
         square.setAttribute('style', 'background-color: black;');
         drawing = true;
     });
-
+    
     square.addEventListener('mousemove', () => {
         if (drawing) {
             square.setAttribute('style', 'background-color: black;');
         }
     });
-
+    
     square.addEventListener('mouseup', () => {
         drawing = false;
     });
-      
+    
     const squares = [input];
+    
+    container.style.gridTemplateColumns = 'repeat(' + input + ', auto)';
+    container.style.gridTemplateRows = 'repeat(' + input + ', auto)';
+    
 
     for (let i = 0; i <= input * input - 2; i++) {
         squares[i] = document.createElement('div');
         squares[i].classList.add('square');
         container.appendChild(squares[i]);
-
+        
         squares[i].addEventListener('mousedown', () => {
             squares[i].setAttribute('style', 'background-color: black;');
             drawing = true;
         });
-
+        
         squares[i].addEventListener('mousemove', () => {
             if (drawing) {
                 squares[i].setAttribute('style', 'background-color: black;');
             }
         });
-
+        
         squares[i].addEventListener('mouseup', () => {
             drawing = false;
-        });
-    }   
+        });        
+    }
 }
+
+createGrid(5);
+button.addEventListener('click', setDimension);
